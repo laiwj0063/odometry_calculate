800ea03e91c1b86066117445ee5e799af4034b3f
laiwj0063 <laiwj0063@gmail.com>
2024-06-25 17:37:07 +0800

    change to two subscriber

--- src/odom_calculate.cpp
+++ src/odom_calculate.cpp
@@ -3,7 +3,7 @@
 #include <geometry_msgs/Point.h>
 #include <tf/transform_broadcaster.h>
 #include <std_msgs/String.h>
-#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Float64.h>
 #include <iostream>
 #include <cstdint>
 #include <vector>
@@ -21,57 +21,17 @@ using std::cout;
 typedef struct {
   double x;
   double y;
-  double z;
+  double th;
 }Position;
 
+typedef struct{
+    double omega_L;
+    double omega_R;
+}Velocity;
+
 Position position;
-void caculate(double,double);
-
-class Publisher{
-public:
-    Publisher(){
-        pub = n.advertise<geometry_msgs::Point>("car_position", 1);
-    }
-
-    void publish(){
-        geometry_msgs::Point msg;
-        msg.x = position.x;
-        msg.y = position.y;
-        msg.z = position.z;
-        pub.publish(msg);
-    }
-
-private:
-    ros::NodeHandle n;
-    ros::Publisher pub;
-};
-
-class Subscriber{
-public:
-    Subscriber(){
-        ros::NodeHandle n;
-        sub = n.subscribe("topic_name", 1, &Subscriber::callback, this);
-
-        // create a Publisher object for republishing
-        republisher = new Publisher();
-    }
-
-    ~Subscriber(){
-        delete republisher;
-    }
-
-    void callback(const std_msgs::Float64MultiArray::ConstPtr& msg){
-
-        double omega_L = msg->data[0];
-        double omega_R = msg->data[1];
-        caculate(omega_L,omega_R);
-        republisher->publish();
-    }
-
-private:
-    ros::Subscriber sub;
-    Publisher* republisher;
-};
+Velocity velocity;
+void caculate(Velocity *velocity, Position* position);
 
 struct MotionState {
     double vx;  // x 方向速度
@@ -79,21 +39,21 @@ struct MotionState {
     double omega; // 角速度
 };
 
-MotionState calculateMotion(double omega_L, double omega_R, double R, double L, double theta, double dt) {//L是兩輪間距
+MotionState calculateMotion(Velocity* velocity, double R, double L, Position* position, double dt) {//L是兩輪間距
     // 计算左右轮的线速度
-    double v_L = omega_L * R;
-    double v_R = omega_R * R;
+    double v_L = velocity->omega_L * R;
+    double v_R = velocity->omega_R * R;
 
     // 计算车辆的线速度和角速度
     double v = (v_L + v_R) / 2;
     double omega = (v_R - v_L) / L;
 
     // 计算 x 和 y 方向的速度
-    double vx = v * cos(theta);
-    double vy = v * sin(theta);
+    double vx = v * cos(position->th);
+    double vy = v * sin(position->th);
 
     // 更新航向角
-    double new_theta = theta + omega * dt;
+    double new_theta = position->th + omega * dt;
 
     #ifdef DEBUG
     cout<<"vx is "<<vx<<", vy is "<<vy<<", omega_z is "<<omega<<"\n";
@@ -103,7 +63,14 @@ MotionState calculateMotion(double omega_L, double omega_R, double R, double L, 
     return {vx, vy, omega};
 }
 
-void caculate(double omega_L, double omega_R){
+void callbackleft(const std_msgs::Float64::ConstPtr& data){
+    velocity.omega_L = data->data;
+}
+void callbackright(const std_msgs::Float64::ConstPtr& data){
+    velocity.omega_R = data->data;
+}
+
+void caculate(Velocity* velocity, Position* position){
 
     ros::Time current_time, last_time;
     current_time = ros::Time::now();
@@ -120,20 +87,20 @@ void caculate(double omega_L, double omega_R){
     double dt = (current_time - last_time).toSec();       
 
     #ifdef DEBUG
-    cout<<"omega_L is "<<omega_L<<"\n";
-    cout<<"omega_R is "<<omega_R<<"\n";
+    cout<<"omega_L is "<<velocity->omega_L<<"\n";
+    cout<<"omega_R is "<<velocity->omega_R<<"\n";
     #endif
 
     // Calculate motion state
-    MotionState motion = calculateMotion(omega_L, omega_R, R, L, position.z, dt);
+    MotionState motion = calculateMotion(velocity, R, L, position, dt);
 
     // Update position and orientation
-    position.x += motion.vx * dt;
-    position.y += motion.vy * dt;
-    position.z += motion.omega * dt;
+    position->x += motion.vx * dt;
+    position->y += motion.vy * dt;
+    position->th += motion.omega * dt;
 
     #ifdef DEBUG
-    cout<<"x is "<<position.x<<", y is "<<position.y<<", theta_z is "<<position.z<<"\n";
+    cout<<"x is "<<position->x<<", y is "<<position->y<<", theta_z is "<<position->th<<"\n";
     cout<<"-------------------------------------------------------------\n";
     #endif
 
@@ -145,10 +112,20 @@ int main(int argc, char** argv){
 
     position.x = 0.0;
     position.y = 0.0;
-    position.z = 0.0;
+    position.th = 0.0;
 
-    Subscriber sub;
-    ros::spin();
-
+    ros::NodeHandle n;
+    ros::Subscriber subleft = n.subscribe("left_velocity", 1, callbackleft);
+    ros::Subscriber subright = n.subscribe("right_velocity", 1, callbackright);
+    ros::Publisher pub = n.advertise<geometry_msgs::Point>("car_position", 1);
+    while (ros::ok()){
+        caculate(&velocity,&position);
+        geometry_msgs::Point msg;
+        msg.x = position.x;
+        msg.y = position.y;
+        msg.z = position.th;
+        pub.publish(msg);
+        ros::spinOnce();
+    }
     return 0;
 }
